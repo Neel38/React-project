429c7dc5bf53062c18ce194d86c2546ca7f980d7
Neel38 <neeltundiya5454@gmail.com>
2024-02-13 22:45:56 +0530

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-900">
+      Hello
+      {/* <div className="w-44 h-32 bg-red-600">XYZ</div> */}
+    </div>
+  )
+} 
+
+export default App;
